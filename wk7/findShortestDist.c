38f7d3f13bfcbf0b323064104a8d19c647bd6dd4
Josh <joshconstantinelim@gmail.com>
2025-04-02 12:08:24 +1100

    wk7

--- wk7/findShortestDist.c
+++ wk7/findShortestDist.c
@@ -9,6 +9,26 @@
 // distance between src and each vertex.
 // For an undirected, unweighted graph.
 int *findShortestDistance(Graph g, int src) {
-    // TODO
-    return 0;
+    Queue q = QueueNew();
+    bool *visited = calloc(GraphNumVertices(g), sizeof(bool));
+    int *dist = calloc(GraphNumVertices(g), sizeof(int));
+
+    QueueEnqueue(q, src);
+    visited[src] = true;
+
+    while (!QueueIsEmpty(q)) {
+        Vertex v = QueueDequeue(q);
+
+        for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+            if (GraphIsAdjacent(g, v, w) && !visited[w]) {
+                QueueEnqueue(q, w);
+                visited[w] = true;
+                dist[w] = dist[v] + 1;
+            }
+        }
+    }
+
+    QueueFree(q);
+    free(visited);
+    return dist;
 }
